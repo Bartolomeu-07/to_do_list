de915f7fa775d062bbb246f8c1d8b0e12e2c788a
Bartolomeu <bartoszokroj@wp.pl>
2025-10-17 22:12:02 +0200

    solution

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,33 @@
 from django.urls import path
 
-from app.views import HomeView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskToggleView, TagListView, \
-    TagUpdateView, TagCreateView, TagDeleteView
+from app.views import (
+    HomeView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskToggleView,
+    TagListView,
+    TagUpdateView,
+    TagCreateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("tasks/<int:pk>/toggle/", TaskToggleView.as_view(), name="task-toggle"),
+    path(
+        "tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/", TaskToggleView.as_view(), name="task-toggle"
+    ),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
-app_name = 'app'
+app_name = "app"
--- app/views.py
+++ app/views.py
@@ -8,41 +8,45 @@ from app.models import Task, Tag
 
 class HomeView(generic.ListView):
     model = Task
-    template_name = 'app/index.html'
-    context_object_name = 'tasks'
+    template_name = "app/index.html"
+    context_object_name = "tasks"
 
     def get_queryset(self):
-        return Task.objects.order_by('done', '-datetime')
+        return Task.objects.order_by("done", "-datetime")
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = '__all__'
-    template_name = 'app/task_form.html'
-    success_url = reverse_lazy('app:home')
+    fields = "__all__"
+    template_name = "app/task_form.html"
+    success_url = reverse_lazy("app:home")
 
     def get_form(self, *args, **kwargs):
         form = super().get_form(*args, **kwargs)
-        form.fields["deadline"].widget = forms.DateInput(attrs={"type": "date", "class": "form-control"})
+        form.fields["deadline"].widget = forms.DateInput(
+            attrs={"type": "date", "class": "form-control"}
+        )
         return form
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = '__all__'
-    template_name = 'app/task_form.html'
-    success_url = reverse_lazy('app:home')
+    fields = "__all__"
+    template_name = "app/task_form.html"
+    success_url = reverse_lazy("app:home")
 
     def get_form(self, *args, **kwargs):
         form = super().get_form(*args, **kwargs)
-        form.fields["deadline"].widget = forms.DateInput(attrs={"type": "date", "class": "form-control"})
+        form.fields["deadline"].widget = forms.DateInput(
+            attrs={"type": "date", "class": "form-control"}
+        )
         return form
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
-    template_name = 'app/task_delete.html'
-    success_url = reverse_lazy('app:home')
+    template_name = "app/task_delete.html"
+    success_url = reverse_lazy("app:home")
 
 
 class TaskToggleView(View):
@@ -56,25 +60,25 @@ class TaskToggleView(View):
 
 class TagListView(generic.ListView):
     model = Tag
-    template_name = 'app/tag_list.html'
-    context_object_name = 'tags'
+    template_name = "app/tag_list.html"
+    context_object_name = "tags"
 
 
 class TagUpdateView(generic.UpdateView):
     model = Tag
-    fields = '__all__'
-    template_name = 'app/tag_form.html'
-    success_url = reverse_lazy('app:tag-list')
+    fields = "__all__"
+    template_name = "app/tag_form.html"
+    success_url = reverse_lazy("app:tag-list")
 
 
 class TagCreateView(generic.CreateView):
     model = Tag
-    fields = '__all__'
-    template_name = 'app/tag_form.html'
-    success_url = reverse_lazy('app:tag-list')
+    fields = "__all__"
+    template_name = "app/tag_form.html"
+    success_url = reverse_lazy("app:tag-list")
 
 
 class TagDeleteView(generic.DeleteView):
     model = Tag
-    template_name = 'app/tag_delete.html'
-    success_url = reverse_lazy('app:tag-list')
+    template_name = "app/tag_delete.html"
+    success_url = reverse_lazy("app:tag-list")
